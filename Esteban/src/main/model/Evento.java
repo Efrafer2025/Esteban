52c484de62b57b504f43f6730ace5e33ae3ad146
Efrafer2025 <est.esparza@duocuc.cl>
2026-04-15 13:03:07 -0400

    Update Evento.java

--- Esteban/src/main/model/Evento.java
+++ Esteban/src/main/model/Evento.java
@@ -1,7 +1,7 @@
 import jakarta.validation.constraints.NotBlank;
 
 public class Evento {
-    
+    private Long id;
     private String nombre;
     private String tipoEvento;
     private int fechaRealizacion;
