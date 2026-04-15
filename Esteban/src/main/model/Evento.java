abb518663d4d991b09b1652a2f003c65ae6cc956
Efrafer2025 <est.esparza@duocuc.cl>
2026-04-15 14:26:35 -0400

    Update Evento.java

--- Esteban/src/main/model/Evento.java
+++ Esteban/src/main/model/Evento.java
@@ -3,10 +3,36 @@ import jakarta.validation.constraints.NotBlank;
 public class Evento {
     private Long id;
     private String nombre;
-    private String tipoEvento;
-    private int fechaRealizacion;
+    private String tipo;
+    private String fecha;
     private String ubicacion;
-    private int capacidadCatidad;
-}   
+    private int capacidad;
+
+    public Evento(){}
     
-}
+    public Evento(Long id,String nombre,String tipo,String fecha,String ubicacion,int capacidad){
+        this.id=id;
+        this.nombre=nombre;
+        this.tipo=tipo;
+        this.fecha=fecha;
+        this.ubicacion=ubicacion;
+        this.capacidad=capacidad;
+    }
+    public Long getId() {return id;}
+    public void setId(Long id) {this.id = id;}
+
+    public String getNombre() {return nombre;}
+    public void setNombre(String nombre) {this.nombre = nombre;}
+
+    public String getTipo() {return tipo;}
+    public void setTipo(String tipo) {this.tipo = tipo;}
+
+    public String getFecha() {return fecha;}
+    public void setFecha(String fecha) {this.fecha = fecha;}
+
+    public String getUbicacion() {return ubicacion;}
+    public void setUbicacion(String ubicacion) {this.ubicacion = ubicacion;}
+
+    public int getCapacidad() {return capacidad;}
+    public void setCapacidad(int capacidad) {this.capacidad = capacidad;}
+}   
